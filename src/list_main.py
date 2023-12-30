7538fb9c1354299ee460c2a8e83fc43c5b791287
casarin <nsgbanks@ciencias.unam.mx>
2023-12-29 19:42:48 -0600

    Node as inner class (Problem with slots fixed)

--- src/list_main.py
+++ src/list_main.py
@@ -7,14 +7,14 @@ print("Lista antes de agregar:")
 print(list.getLength())
 print(list)
 
-list.addAt(element, 0)
+list.add_at(element, 0)
 
 print("Lista despues de agregar un elemento:")
 print(list.getLength())
 print(list)
 
 for i in range(5):
-    list.addAt(Element("inicio"), 0)
+    list.add_at(Element("inicio"), 0)
 
 print("Lista despues de agregar multiples elementos al inicio:")
 print(list.getLength())
--- src/my_list.py
+++ src/my_list.py
@@ -10,57 +10,32 @@ class Element():
     def __str__(self):
         return self.__value
 
-class Node:
-    def __init__(self):
-        self.prev = None
-        self.next = None
-        self.element = None
-
-    def set_element(self, element):
-        self.element = element
-
-    def get_element(self):
-        return self.element
-
-    def get_next(self):
-        return self.next
-
-    def set_next(self, next_node):
-        self.next = next_node
-
-    def get_prev(self):
-        return self.prev
-
-    def set_prev(self, prev_node):
-        self.prev = prev_node
-
-
 class List():
     __slots__ = ("__head", "__tail", "__length")
 
-    class Node:
+    class Node():
         def __init__(self):
-            self.prev = None
-            self.next = None
-            self.element = None
+            self.__prev = None
+            self.__next = None
+            self.__element = None
 
         def set_element(self, element):
-            self.element = element
+            self.__element = element
 
         def get_element(self):
-            return self.element
+            return self.__element
 
         def get_next(self):
-            return self.next
+            return self.__next
 
         def set_next(self, next_node):
-            self.next = next_node
+            self.__next = next_node
 
         def get_prev(self):
-            return self.prev
+            return self.__prev
             
         def set_prev(self, prev_node):
-            self.prev = prev_node
+            self.__prev = prev_node
 
     def __init__(self):
         self.__head = None
@@ -71,7 +46,7 @@ class List():
         if not isinstance(element, Element) or i < 0 or i > self.__length:
             raise ValueError("It is not possible to add this element to the list.")
 
-        node = Node()
+        node = List.Node()
         node.set_element(element)
 
         if self.__length == 0:
@@ -88,7 +63,7 @@ class List():
             else:
                 change = self.get_node_at(i)
                 node.set_prev(change.get_prev)
-                change.__prev.set_next(node)
+                change.get_prev().set_next(node)
                 node.set_next(change)
                 change.set_prev(node)
                 
@@ -112,16 +87,16 @@ class List():
     def getLength(self):
         return self.__length
 
-    def __str__(self):
-        if self.is_empty():
-            return "[]"
-        it = self.__head
-        string = "[ "
-        string += it.get_element().__str__()
-        it = it.get_next()
-        for i in range(self.__length):
-            string += ", " + it.get_element().__str__()
-        string += "]"
-        return string
+    # def __str__(self):
+    #     if self.is_empty():
+    #         return "[]"
+    #     it = self.__head
+    #     string = "[ "
+    #     string += it.get_element().__str__()
+    #     it = it.get_next()
+    #     for i in range(self.__length):
+    #         string += ", " + it.get_element().__str__()
+    #     string += "]"
+    #     return string
     
 
